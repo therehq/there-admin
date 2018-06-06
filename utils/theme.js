6ccfe3c772386f9b434fd68541579949de67b719
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-06-06 14:52:50 +0430

    Add theme project defaults

---
+++ utils/theme.js
@@ -0,0 +1,11 @@
+export default {
+  //fonts
+  mainFont:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif',
+
+  //sizes
+  innerPageSidePadding: 32,
+
+  //colors
+  mainBgColor: '#E5E5E5',
+}
